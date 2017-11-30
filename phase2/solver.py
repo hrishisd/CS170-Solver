e063d652e84c290e54f55c431e55ff68854c9944
Kevin Bunarjo <kbunarjo@berkeley.edu>
2017-11-30 15:30:20 -0800

    yadada

--- phase2/solver.py
+++ phase2/solver.py
@@ -23,64 +23,113 @@ def solve(num_wizards, num_constraints, wizards, constraints):
     Output:
         An array of wizard names in the ordering your algorithm returns
     """
+    #construct edges (edge is called w1;w2 iff w1 < w2)
+    E = {}
+    for i in range(num_wizards):
+        for j in range(i + 1, num_wizards):
+            w1, w2 = wizards[i], wizards[j]
+            if w1 < w2:
+                E[(w1, w2)] = 0
+            else:
+                E[(w2, w1)] = 0
+    #increment the counts
+    for constraint in constraints:
+        w1, w2, w3 = constraint[0], constraint[1], constraint[2]
+        #do it for w1 and w3
+        if w1 < w3:
+            E[(w1, w3)] += 1
+        else:
+            E[(w3, w1)] += 1
+        #do it for w2 and w3
+        if w2 < w3:
+            E[(w2, w3)] += 1
+        else:
+            E[(w3, w2)] += 1
+    #filter edges based on constraints
+    threshold = num_wizards - 2
+    filteredE = set()
+    for edge in E:
+        if E[edge] <= threshold:
+            filteredE.add(edge)
 
-    def map_wiz_to_index(partial_ordering):
-        mapping = {}
-        for i, wiz in enumerate(partial_ordering):
-            mapping[wiz] = i
+    #get the degrees
+    degreeDict = {}
+    for wiz in wizards:
+        degreeDict[wiz] = 0
+    for w1, w2 in filteredE:
+        degreeDict[w1] += 1
+        degreeDict[w2] += 1
 
-        return mapping
+    #associate to each edge to number of neighbors (reinitialize E)
+    E = {}
+    for i in range(num_wizards):
+        for j in range(i + 1, num_wizards):
+            w1, w2 = wizards[i], wizards[j]
+            if w1 < w2:
+                E[(w1, w2)] = degreeDict[w1] + degreeDict[w2]
+            else:
+                E[(w2, w1)] = degreeDict[w1] + degreeDict[w2]
 
-    def count_invalid_constraints(partial_ordering):
-        wiz_to_invalid = map_wiz_to_invalid_constraint(partial_ordering)
-        count = sum(wiz_to_invalid.values())
-        return count/3
+    #get rid of highest element. redo procedure
+    sortedE = sorted(E, key=lambda k: E[k])
+    sortedE = sortedE[0:len(sortedE)-1]
+    while (len(sortedE) > num_wizards - 1):
+        degreeDict = {}
+        for wiz in wizards:
+            degreeDict[wiz] = 0
+        for w1, w2 in sortedE:
+            degreeDict[w1] += 1
+            degreeDict[w2] += 1
+        E = {}
+        for edge in sortedE:
+            E[edge] = degreeDict[edge[0]] + degreeDict[edge[1]]
+        sortedE = sorted(E, key=lambda k: E[k])
+        sortedE = sortedE[0:len(sortedE)-2]
 
-    def map_wiz_to_invalid_constraint(partial_ordering):
-        wiz_to_index = map_wiz_to_index(partial_ordering)
-
-        mapping = defaultdict(int)
-    
-        for constraint in constraints:
-            w1, w2, w3 = constraint[0], constraint[1], constraint[2]
-            i, j, k = wiz_to_index[w1], wiz_to_index[w2], wiz_to_index[w3]
-            if (k > i and k < j) or (k < i and k > j):
-                mapping[w1] += 1
-                mapping[w2] += 1
-                mapping[w3] += 1
-        return mapping
-
-    def get_most_invalid_wiz(partial_ordering):
-        #get mapping from wiz to num invalid
-        wiz_to_invalid = map_wiz_to_invalid_constraint(partial_ordering)
-
-        #get wiz with max invalid count 
-        max_wiz = max(wiz_to_invalid.iteritems(), key=operator.itemgetter(1))[0]
-
-        return max_wiz
-
-        
-    best = wizards
-    best_invalid = count_invalid_constraints(wizards)
-    print ("num constraints", num_constraints)
-    print("init invalid", best_invalid)
-    while(best_invalid > 10):
-        temp = np.random.permutation(wizards)
-        curr_invalid = count_invalid_constraints(temp)
-        #print(curr_invalid)
-        if (curr_invalid < best_invalid):
-            best_invalid = curr_invalid
-            best = temp
-            print(best_invalid)
-            print(best)
-
-        if best_invalid == 0:
+    #sort edges into path
+    ordering = []
+    degreeDict = {}
+    for wiz in wizards:
+        degreeDict[wiz] = 0
+    for w1, w2 in sortedE:
+        degreeDict[w1] += 1
+        degreeDict[w2] += 1
+    nextWiz = ""
+    for edge in sortedE:
+        if degreeDict[edge[0]] == 1:
+            ordering.append(edge[0])
+            ordering.append(edge[1])
+            nextWiz = edge[1]
+            sortedE.remove(edge)
             break
+        elif degreeDict[edge[1]] == 1:
+            ordering.append(edge[1])
+            ordering.append(edge[0])
+            nextWiz = edge[0]
+            sortedE.remove(edge)
+            break
+    while len(sortedE) > 1:
+        print(len(sortedE))
+        for edge in sortedE:
+            if edge[0] == nextWiz:
+                ordering.append(edge[1])
+                nextWiz = edge[1]
+                sortedE.remove(edge)
+                break
+            elif edge[1] == nextWiz:
+                ordering.append(edge[0])
+                nextWiz = edge[0]
+                sortedE.remove(edge)
+                break
+    for e in sortedE:
+        if e[0] == nextWiz:
+            ordering.append(e[1])
+        else:
+            ordering.append(e[0])
+    print("sorted: ", ordering)
+    return ordering
 
 
-
-    #print(constraints, constraints)
-    return best
 
 """
 ======================================================================
