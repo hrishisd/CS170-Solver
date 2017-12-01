68b3b18672deed040523937891bda3a10fac0cbe
Kevin Bunarjo <kbunarjo@berkeley.edu>
2017-11-30 16:48:05 -0800

    yay

--- phase2/solver.py
+++ phase2/solver.py
@@ -23,6 +23,81 @@ def solve(num_wizards, num_constraints, wizards, constraints):
     Output:
         An array of wizard names in the ordering your algorithm returns
     """
+
+    def map_wiz_to_index(partial_ordering):
+        mapping = {}
+        for i, wiz in enumerate(partial_ordering):
+            mapping[wiz] = i
+
+        return mapping
+
+    def count_invalid_constraints(partial_ordering):
+        wiz_to_invalid = map_wiz_to_invalid_constraint(partial_ordering)
+        count = sum(wiz_to_invalid.values())
+        return count/3
+
+    def map_wiz_to_invalid_constraint(partial_ordering):
+        wiz_to_index = map_wiz_to_index(partial_ordering)
+
+        mapping = defaultdict(int)
+
+        for constraint in constraints:
+            w1, w2, w3 = constraint[0], constraint[1], constraint[2]
+            i, j, k = wiz_to_index[w1], wiz_to_index[w2], wiz_to_index[w3]
+            if (k > i and k < j) or (k < i and k > j):
+                mapping[w1] += 1
+                mapping[w2] += 1
+                mapping[w3] += 1
+        return mapping
+
+    def get_most_invalid_wiz(partial_ordering):
+        #get mapping from wiz to num invalid
+        wiz_to_invalid = map_wiz_to_invalid_constraint(partial_ordering)
+
+        #get wiz with max invalid count
+        max_wiz = max(wiz_to_invalid.items(), key=operator.itemgetter(1))[0]
+
+        return max_wiz
+
+    def update(partial_ordering):
+        """
+        returns a better ordering from 1 swap
+        """
+        max_wiz = get_most_invalid_wiz(partial_ordering)
+        bad_wiz_index = partial_ordering.index(max_wiz)
+        curr_num_invalid = count_invalid_constraints(partial_ordering)
+        for i in range(num_wizards):
+            if i == bad_wiz_index:
+                pass
+            partial_ordering[i], partial_ordering[bad_wiz_index] = partial_ordering[bad_wiz_index], partial_ordering[i]
+            if count_invalid_constraints(partial_ordering) < curr_num_invalid:
+                return list(partial_ordering)
+            else:
+                partial_ordering[i], partial_ordering[bad_wiz_index] = partial_ordering[bad_wiz_index], partial_ordering[i]
+        return list(partial_ordering)
+
+    #def random_update(partial_ordering):
+
+
+    best = KevinSort(num_wizards, num_constraints, wizards, constraints)
+    best_invalid = count_invalid_constraints(best)
+    updated_ordering = best
+    prev_num_invalid = count_invalid_constraints(updated_ordering)
+    while (best_invalid > 10):
+        updated_ordering = update(updated_ordering)
+        curr_num_invalid = count_invalid_constraints(updated_ordering)
+        print("alvin",curr_num_invalid)
+        if curr_num_invalid == prev_num_invalid:
+            pass
+
+        prev_num_invalid = curr_num_invalid
+
+
+
+    #print(constraints, constraints)
+    return updated_ordering
+
+def KevinSort(num_wizards, num_constraints, wizards, constraints):
     #construct edges (edge is called w1;w2 iff w1 < w2)
     E = {}
     for i in range(num_wizards):
@@ -109,7 +184,7 @@ def solve(num_wizards, num_constraints, wizards, constraints):
             sortedE.remove(edge)
             break
     while len(sortedE) > 1:
-        print(len(sortedE))
+        print("kevin",len(sortedE))
         for edge in sortedE:
             if edge[0] == nextWiz:
                 ordering.append(edge[1])
